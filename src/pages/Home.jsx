9ca61ac9cf12ba901a3b7a91201936320bcd5706
Andrej Simko <4717664+Hipapheralkus@users.noreply.github.com>
2025-07-27 13:40:45 +0200

    Aktivni Mesto

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,14 @@ import BannerVideo from '../components/BannerVideo';
 import About from '../components/About';
 import YouTubeVideos from '../components/YouTubeVideos';
 import Gallery from '../components/Gallery';
+import AktivniMestoBanner from '../components/AktivniMestoBanner.jsx';
 import './Home.css';
 
 const Home = () => {
   return (
     <div className="home">
       <BannerVideo />
+      <AktivniMestoBanner />
       <div className="home-container">
         <About />
         <YouTubeVideos />
